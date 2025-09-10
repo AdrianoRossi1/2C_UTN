372f60d163d0482a7350e693b6659f60695fd494
AdrianoRossi1 <rossiadriano315@gmail.com>
2025-09-10 02:11:21 -0300

    Update act_10.py

--- semana_3/guia_vectores/act_10.py
+++ semana_3/guia_vectores/act_10.py
@@ -1,16 +1,12 @@
 def buscar_numero(array, numero):
-    if numero in array:
-        return array.index(numero)
-    else:
-        return -1
+    for i in range(len(array)): 
+        if array[i] == numero:   
+            return i+1           
+    return -1                  
 
 numeros = [int(input(f"Ingrese el número {i+1}: ")) for i in range(10)]
 
 num_a_buscar = int(input("Ingrese el número a buscar: "))
 
 posicion = buscar_numero(numeros, num_a_buscar)
-if posicion != -1:
-    print(f"El número se encuentra en la posición {posicion}.")
-else:
-    print(posicion)
-
+print(posicion) 
